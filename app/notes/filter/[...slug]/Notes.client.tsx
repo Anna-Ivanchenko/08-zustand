9cfe12f40f554b63c7ebe12c1cebf11210519a85
Anna Ivanchenko <bubachka1990@gmail.com>
2026-06-02 17:44:15 +0300

    fix

--- app/notes/filter/[...slug]/Notes.client.tsx
+++ app/notes/filter/[...slug]/Notes.client.tsx
@@ -1,26 +1,36 @@
 'use client';
 import 'modern-normalize/modern-normalize.css';
+
+import { useState } from 'react';
 import { useQuery, keepPreviousData } from '@tanstack/react-query';
+import { useDebouncedCallback } from 'use-debounce';
+import { Toaster } from 'react-hot-toast';
+
 import css from './Notes.module.css';
+
 import Pagination from '../../../../components/Pagination/Pagination';
 import SearchBox from '../../../../components/SearchBox/SearchBox';
 import Modal from '../../../../components/Modal/Modal';
 import NoteForm from '../../../../components/NoteForm/NoteForm';
-import { useState } from 'react';
-import { fetchNotes } from '../../../../lib/api';
 import NoteList from '../../../../components/NoteList/NoteList';
-import { useDebouncedCallback } from 'use-debounce';
-import { Toaster } from 'react-hot-toast';
+
+import { fetchNotes } from '../../../../lib/api';
 import { NoteTag } from '@/types/note';
 
 type Props = {
   tag?: NoteTag;
 };
+
 function App({ tag }: Props) {
   const [createNoteThis, setCreateNoteThis] = useState(false);
   const [input, setInput] = useState('');
   const [querySe, setQuery] = useState('');
   const [page, setPage] = useState(1);
+
+  const debouncedSetQuery = useDebouncedCallback((value: string) => {
+    setQuery(value);
+    setPage(1);
+  }, 500);
 
   const { data, isLoading, isSuccess, isFetching } = useQuery({
     queryKey: ['notes', { page, querySe, tag }],
@@ -29,14 +39,10 @@ function App({ tag }: Props) {
         page,
         search: querySe || undefined,
         perPage: 12,
-        tag: tag,
+        tag,
       }),
     placeholderData: keepPreviousData,
   });
-
-  const debouncedSetQuery = useDebouncedCallback((value: string) => {
-    setQuery(value);
-  }, 500);
 
   const openModal = () => {
     setCreateNoteThis(true);
@@ -56,34 +62,47 @@ function App({ tag }: Props) {
       <header className={css.toolbar}>
         <SearchBox
           value={input}
-          onChange={val => {
+          onChange={(val) => {
             setInput(val);
             debouncedSetQuery(val);
-            setPage(1);
           }}
         />
 
         {isSuccess && totalPages > 1 && (
-          <Pagination totalPages={totalPages} page={page} setPage={setPage} />
+          <Pagination
+            totalPages={totalPages}
+            page={page}
+            setPage={setPage}
+          />
         )}
 
-        <button className={css.button} onClick={openModal}>
+        <button
+          className={css.button}
+          onClick={openModal}
+        >
           Create note +
         </button>
       </header>
 
       {isEmpty ? (
         <p>
-          {tag || querySe ? 'No notes found for this filter' : 'No notes yet'}
+          {tag || querySe
+            ? 'No notes found for this filter'
+            : 'No notes yet'}
         </p>
       ) : (
         !isLoading &&
         !isFetching &&
         isSuccess &&
-        data.notes.length > 0 && <NoteList notes={data.notes} />
+        notes.length > 0 && (
+          <NoteList notes={notes} />
+        )
       )}
 
-      <Toaster position="top-center" reverseOrder={false} />
+      <Toaster
+        position="top-center"
+        reverseOrder={false}
+      />
 
       {createNoteThis && (
         <Modal onClose={closeModal}>
